b63f9fc9dab4c90ee9c3ec1d07f52ad5a71ec7f4
bjsi <experimentallearning0@gmail.com>
2021-04-12 00:55:54 +0100

    - Added: Import epub

---
+++ src/SuperMemoAssistant.Plugins.EpubImporter/EpubImporterCfg.cs
@@ -0,0 +1,106 @@
+﻿using Forge.Forms;
+using Forge.Forms.Annotations;
+using Newtonsoft.Json;
+using SuperMemoAssistant.Services.UI.Configuration;
+using SuperMemoAssistant.Sys.ComponentModel;
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace SuperMemoAssistant.Plugins.EpubImporter
+{
+  [Form(Mode = DefaultFields.None)]
+  [Title("Dictionary Settings",
+   IsVisible = "{Env DialogHostContext}")]
+  [DialogAction("cancel",
+  "Cancel",
+  IsCancel = true)]
+  [DialogAction("save",
+  "Save",
+  IsDefault = true,
+  Validates = true)]
+  public class EpubImporterCfg : CfgBase<EpubImporterCfg>, INotifyPropertyChangedEx
+  {
+    [Title("Epub Importer Plugin")]
+    [Heading("By Jamesb | Experimental Learning")]
+
+    [Heading("Features")]
+    [Text(@"- Import epub books into SuperMemo.")]
+
+    [Heading("Support")]
+    [Text("If you would like to support my projects, check out my Patreon or buy me a coffee.")]
+
+    [Action("patreon", "Patreon", Placement = Placement.Before, LinePosition = Position.Left)]
+    [Action("coffee", "Coffee", Placement = Placement.Before, LinePosition = Position.Left)]
+
+    [Heading("Links")]
+    [Action("github", "GitHub", Placement = Placement.Before, LinePosition = Position.Left)]
+    [Action("feedback", "Feedback Site", Placement = Placement.Before, LinePosition = Position.Left)]
+    [Action("blog", "Blog", Placement = Placement.Before, LinePosition = Position.Left)]
+    [Action("youtube", "YouTube", Placement = Placement.Before, LinePosition = Position.Left)]
+    [Action("twitter", "Twitter", Placement = Placement.Before, LinePosition = Position.Left)]
+
+    [Heading("Settings")]
+    [Field(Name = "Default Priority?")]
+    [Value(Must.BeGreaterThanOrEqualTo,
+           0,
+           StrictValidation = true)]
+    [Value(Must.BeLessThanOrEqualTo,
+           100,
+           StrictValidation = true)]
+    public double DefaultPriority { get; set; } = 30;
+
+    [JsonIgnore]
+    public bool IsChanged { get; set; }
+
+    public override string ToString()
+    {
+      return "Epub Importer Settings";
+    }
+
+    public override void HandleAction(IActionContext actionContext)
+    {
+
+      string patreon = "https://www.patreon.com/experimental_learning";
+      string coffee = "https://buymeacoffee.com/experilearning";
+      string github = "https://github.com/bjsi/SuperMemoAssistant.Plugins.HtmlTables";
+      string feedback = "https://feedback.experimental-learning.com/";
+      string youtube = "https://www.youtube.com/channel/UCIaS9XDdQkvIjASBfgim1Uw";
+      string twitter = "https://twitter.com/experilearning";
+      string blog = "https://www.experimental-learning.com/";
+
+      string action = actionContext.Action as string;
+      if (action == "patreon")
+        openLinkDefaultBrowser(patreon);
+      else if (action == "github")
+        openLinkDefaultBrowser(github);
+      else if (action == "coffee")
+        openLinkDefaultBrowser(coffee);
+      else if (action == "feedback")
+        openLinkDefaultBrowser(feedback);
+      else if (action == "youtube")
+        openLinkDefaultBrowser(youtube);
+      else if (action == "twitter")
+        openLinkDefaultBrowser(twitter);
+      else if (action == "blog")
+        openLinkDefaultBrowser(blog);
+      else
+        base.HandleAction(actionContext);
+    }
+
+    // Hack
+    private DateTime LastLinkOpen { get; set; } = DateTime.MinValue;
+
+    private void openLinkDefaultBrowser(string url)
+    {
+      var diffInSeconds = (DateTime.Now - LastLinkOpen).TotalSeconds;
+      if (diffInSeconds > 1)
+      {
+        LastLinkOpen = DateTime.Now;
+        Process.Start(url);
+      }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+  }
+}
---
+++ src/SuperMemoAssistant.Plugins.EpubImporter/EpubImporterPlugin.cs
@@ -0,0 +1,269 @@
+﻿using Anotar.Serilog;
+using EpubSharp;
+using Microsoft.Win32;
+using SuperMemoAssistant.Interop.Plugins;
+using SuperMemoAssistant.Interop.SuperMemo.Content.Contents;
+using SuperMemoAssistant.Interop.SuperMemo.Elements.Builders;
+using SuperMemoAssistant.Interop.SuperMemo.Elements.Models;
+using SuperMemoAssistant.Interop.SuperMemo.Elements.Types;
+using SuperMemoAssistant.Services;
+using SuperMemoAssistant.Services.IO.HotKeys;
+using SuperMemoAssistant.Services.IO.Keyboard;
+using SuperMemoAssistant.Services.UI.Configuration;
+using SuperMemoAssistant.Sys.IO.Devices;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+#region License & Metadata
+
+// The MIT License (MIT)
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the 
+// Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+// 
+// 
+// Created On:   1/24/2021 11:05:26 AM
+// Modified By:  james
+
+#endregion
+
+
+
+
+namespace SuperMemoAssistant.Plugins.EpubImporter
+{
+  // ReSharper disable once UnusedMember.Global
+  // ReSharper disable once ClassNeverInstantiated.Global
+  [SuppressMessage("Microsoft.Naming", "CA1724:TypeNamesShouldNotMatchNamespaces")]
+  public class EpubImporterPlugin : SMAPluginBase<EpubImporterPlugin>
+  {
+    #region Constructors
+
+    /// <inheritdoc />
+    public EpubImporterPlugin() { }
+
+    #endregion
+
+
+
+
+    #region Properties Impl - Public
+
+    /// <inheritdoc />
+    public override string Name => "EpubImporter";
+
+    /// <inheritdoc />
+    public override bool HasSettings => true;
+
+    private SemaphoreSlim ImportSemaphore { get; } = new SemaphoreSlim(1, 1);
+
+    public EpubImporterCfg Config { get; private set; }
+
+    #endregion
+
+    private async Task LoadConfig()
+    {
+      Config = await Svc.Configuration.Load<EpubImporterCfg>() ?? new EpubImporterCfg();
+    }
+
+
+
+    #region Methods Impl
+
+    /// <inheritdoc />
+    protected override void PluginInit()
+    {
+
+      LoadConfig().Wait();
+      
+      Svc.HotKeyManager.RegisterGlobal(
+        "ImportEPUB",
+        "Import an Epub file",
+        HotKeyScope.SMBrowser,
+        new HotKey(Key.Y, KeyModifiers.CtrlShift),
+        OpenFile
+        );
+    }
+
+    #endregion
+
+
+    #region Methods
+
+    private void OpenFile()
+    {
+      if (ImportSemaphore.Wait(0) == false)
+        return;
+
+      string filePath = OpenFileDialog();
+
+      if (filePath != null)
+      {
+        ImportEpub(filePath);
+      }
+
+      ImportSemaphore.Release();
+    }
+
+    public void ImportEpub(string filepath)
+    {
+      try
+      {
+        var book = EpubReader.Read(filepath);
+        var parentId = CreateBookFolder(book);
+        var parentEl = Svc.SM.Registry.Element[parentId];
+
+        if (parentId < 1 || parentEl == null)
+        {
+          LogTo.Debug("Failed to create book folder.");
+          return;
+        }
+
+        foreach (var chapter in book.TableOfContents)
+        {
+          if (ImportChapter(parentEl, book, chapter) == -1)
+          {
+            LogTo.Debug("Failed to import chapter.");
+            break;
+          }
+        }
+      }
+      catch (Exception e)
+      {
+        LogTo.Debug($"Failed to import {filepath} with exception {e}");
+      }
+    }
+
+    private int CreateBookFolder(EpubBook book)
+    {
+      return CreateSMFolder(CreateBookReference(book));
+    }
+
+    private References CreateBookReference(EpubBook book)
+    {
+      return new References()
+        .WithAuthor(string.Join(", ", book.Authors))
+        .WithTitle(book.Title);
+    }
+
+    private References CreateChapterReference(EpubBook book, EpubChapter chapter)
+    {
+      return new References()
+        .WithAuthor(string.Join(", ", book.Authors))
+        .WithTitle(book.Title + ": " + chapter.Title);
+    }
+
+    private int ImportChapter(IElement parentFolder, EpubBook book, EpubChapter chapter)
+    {
+      var refs = CreateChapterReference(book, chapter);
+      try
+      {
+        string html = book.Resources.Html.Where(x => Path.GetFileName(x.FileName) == chapter.FileName).First().TextContent;
+        return CreateSMTopic(html, refs, parentFolder);
+      }
+      catch (Exception ex)
+      {
+        LogTo.Debug($"Failed to import chapter with exception {ex}");
+        return -1;
+      }
+    }
+
+    private int CreateSMTopic(string html, References refs, IElement parent)
+    {
+      return CreateSMElement(new List<ContentBase> { new TextContent(true, html) }, refs, false, parent);
+    }
+
+    private int CreateSMFolder(References refs)
+    {
+      return CreateSMElement(new List<ContentBase>(), refs, true, Svc.SM.UI.ElementWdw.CurrentElement);
+    }
+
+    [LogToErrorOnException]
+    private int CreateSMElement(List<ContentBase> contents, References refs, bool dismissed, IElement parent)
+    {
+      if (parent == null)
+      {
+        LogTo.Error("Failed to CreateSMElement because parent was null");
+        return -1;
+      }
+
+      bool success = Svc.SM.Registry.Element.Add(
+        out var val,
+        ElemCreationFlags.ForceCreate,
+        new ElementBuilder(ElementType.Topic, contents.ToArray())
+          .WithPriority(Config.DefaultPriority)
+          .WithParent(parent)
+          .WithStatus(dismissed ? ElementStatus.Dismissed : ElementStatus.Memorized)
+          .DoNotDisplay()
+          .WithReference((_) => refs)
+      );
+
+      if (success)
+      {
+        LogTo.Debug("Successfully created SM Element");
+        return val[0].ElementId;
+      }
+      else
+      {
+        LogTo.Error("Failed to CreateSMElement");
+        return -1;
+      }
+    }
+
+
+
+    /// <summary>
+    /// Show a dialog which prompts the user to pick an Epub file to import
+    /// </summary>
+    /// <returns>Filename or null</returns>
+    public static string OpenFileDialog()
+    {
+      OpenFileDialog dlg = new OpenFileDialog
+      {
+        DefaultExt = ".epub",
+        Filter = "EPUB files (*.epub)|*.epub|All files (*.*)|*.*",
+        CheckFileExists = true,
+      };
+
+      bool res = (bool)dlg.GetType()
+          .GetMethod("RunDialog", BindingFlags.NonPublic | BindingFlags.Instance)
+          .Invoke(dlg, new object[] { Svc.SM.UI.ElementWdw.Handle });
+
+      return res //dlg.ShowDialog(this).GetValueOrDefault(false)
+          ? dlg.FileName
+          : null;
+    }
+
+    // Set HasSettings to true, and uncomment this method to add your custom logic for settings
+    /// <inheritdoc />
+    public override void ShowSettings()
+    {
+      ConfigurationWindow.ShowAndActivate(HotKeyManager.Instance, Config);
+    }
+
+
+    #endregion
+  }
+}
